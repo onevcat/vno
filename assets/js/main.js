a9bfaa73236e7b9d9cabbacc4b493c066d3fb618
onevcat <onevcat@gmail.com>
2015-02-20 12:40:29 +0900

    Fix main page animation to meet the modified max width

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
       $('.content-wrapper').addClass('animated slideInRight');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '500px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
     }
   });
 
